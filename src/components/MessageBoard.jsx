94e476bc0be467b1db08be9ba7b4ed827a0d65fb
Athikhom_ra <athikhom_ra@outlook.com>
2024-06-06 13:43:09 +0700

    Assignment 'react manipulating array state' NARu

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,60 @@
+import { useState } from "react";
 function MessageBoard() {
+  const [message, setMessage] = useState([
+    "Hello all ! This is first message.",
+  ]);
+  const [input, setInput] = useState("");
+  const handleOnChangeInput = (e) => {
+    setInput(e.target.value);
+    // console.log(input);
+  };
+  const handleOnClickSubmit = (e) => {
+    e.preventDefault();
+    if (input === "") {
+      alert("Please enter message!!!");
+    } else {
+      setMessage([input, ...message]);
+      setInput("");
+    }
+  };
+  const handleOnClickDelete = (index) => {
+    const newMessage = [...message];
+    newMessage.splice(index, 1);
+    setMessage(newMessage);
+  };
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={input}
+            onChange={handleOnChangeInput}
+            required
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handleOnClickSubmit}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {message.map((value, i) => {
+          return (
+            <div className="message" key={i}>
+              <h1>{value}</h1>
+              <button
+                className="delete-button"
+                onClick={() => handleOnClickDelete(i)}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
